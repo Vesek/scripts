8db26905e1b0eb217bbaf3b17e3916da3cf8d228
vasek <venda.straka@gmail.com>
2020-12-23 19:46:12 +0100

    Complete!

--- debianafk.sh
+++ debianafk.sh
@@ -4,10 +4,13 @@ if (( $EUID != 0 )); then
     exit
 fi
 apt install git python
+cd /usr/local/share
 git clone https://github.com/ammaraskar/pyCraft.git
-cat > afkstart <<EOF
+cat > ~/$3 <<EOF
 #!/bin/sh
-cd ~/pyCraft
+cd /usr/local/share/pyCraft
 python3 start.py -u $1 -p $2 -s simp.majnc.eu:25569
 EOF
-chmod +x afkstart
+chmod ~/$3
+pip3 install cryptography requests PyNBT
+echo Done, run $3 to start the script!
--- termuxafk.sh
+++ termuxafk.sh
@@ -8,3 +8,5 @@ cd ~/pyCraft
 python3 start.py -u $1 -p $2 -s simp.majnc.eu:25569
 EOF
 chmod +x ~/../usr/bin/$3
+pip3 install cryptography requests PyNBT
+echo Done, run $3 to start the script!
