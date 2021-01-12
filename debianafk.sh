d255d9f06928d90aa1e19c884b20913961439330
Vesek <61123478+Vesek@users.noreply.github.com>
2021-01-12 16:25:56 +0100

    Fixed sumfin

--- debianafk.sh
+++ debianafk.sh
@@ -3,13 +3,13 @@ if (( $EUID != 0 )); then
     echo "Please run as root"
     exit
 fi
+apt install git python3 python3-pip
 if [ -d "~/.local/share/pyCraft" ] 
 then
     (cd ~/.local/share/pyCraft && git fetch) 
 else
     git clone https://github.com/ammaraskar/pyCraft.git ~/.local/share/pyCraft
 fi
-apt install git python3 python3-pip
 cat > /usr/bin/$3 <<EOF
 #!/bin/sh
 cd ~/.local/share/pyCraft
